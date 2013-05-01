4d8be6952dfd4aea08527d360e222c9cd7b0dfad
sscnt <pixeltc@gmail.com>
2013-05-01 23:32:46 +0900

    ddd

---
+++ TwitterTrend/UITwitterScrollView.h
@@ -0,0 +1,17 @@
+//
+//  UITwitterScrollView.h
+//  TwitterTrend
+//
+//  Created by SSC on 2013/05/01.
+//  Copyright (c) 2013年 SSC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UITwitterScrollView : UIScrollView
+
+- (void)appendView:(UIView*)view;
+
+- (void)prependView:(UIView*)view;
+
+@end
---
+++ TwitterTrend/UITwitterScrollView.m
@@ -0,0 +1,29 @@
+//
+//  UITwitterScrollView.m
+//  TwitterTrend
+//
+//  Created by SSC on 2013/05/01.
+//  Copyright (c) 2013年 SSC. All rights reserved.
+//
+
+#import "UITwitterScrollView.h"
+
+@implementation UITwitterScrollView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+
+- (void)drawRect:(CGRect)rect
+{
+
+}
+
+
+@end
