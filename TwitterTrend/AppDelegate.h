582a03e4024fa77e8db2a594f5ec02878bd78363
sscnt <pixeltc@gmail.com>
2013-05-01 21:51:30 +0900

    Initial Commit

---
+++ TwitterTrend/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TwitterTrend
+//
+//  Created by SSC on 2013/05/01.
+//  Copyright (c) 2013年 SSC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
